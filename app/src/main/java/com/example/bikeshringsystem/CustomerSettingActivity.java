9df22a3f40ba254718cc4037b65ac6293019ddd7
Shivom@1 <pandeyshivom43@gmail.com>
2020-04-26 10:25:23 -0700

    Customer information added

---
+++ app/src/main/java/com/example/bikeshringsystem/CustomerSettingActivity.java
@@ -0,0 +1,14 @@
+package com.example.bikeshringsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_setting);
+    }
+}
